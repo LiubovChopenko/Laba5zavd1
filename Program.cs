f3a35db5a13f5267484a8c7abfd4f942b5593fd7
User <User@DESKTOP-0FB8NOE>
2024-11-08 14:55:49 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba5
+{
+    internal class Program
+  
+        {
+            static void Main(string[] args)
+            {
+                int n = 8; 
+                int[,] matrix = new int[n, n];
+                Random random = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = random.Next(0, 100);
+                    }
+                }
+                Console.WriteLine("Початкова матриця:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                for (int i = 0; i < n; i++)
+                {
+                    int sum = 0;
+                    if (i > 0) // Діагональ вище головної діагоналі
+                    {
+                        sum += matrix[i - 1, i];
+                    }
+                    if (i < n - 1) // Діагональ нижче головної діагоналі
+                    {
+                        sum += matrix[i + 1, i];
+                    }
+                    matrix[i, i] = sum;
+                }     
+            Console.WriteLine("Матриця після заміни:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+    
+
+        
+    
+
